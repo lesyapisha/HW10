981a8cc3039b1784ecd4c7d7d517deaa26aea661
lesya.pisha <lesya.pisha@gmail.com>
2014-12-19 23:12:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var users = [];
+
+//amount of users
+var amount = prompt('Enter amount of users', 0);
+
+//filling of array
+for (var i=0; i < amount; i++){
+
+	user = prompt('Enter your name', '');
+	users.push(user);
+	console.log(users[i]);
+}
+
+//checking of correct loggining
+var userName = prompt("Enter your login, please.");
+
+for (var i=0; i<users.length; i++){
+
+	if (userName == users[i]){
+		alert('Logining succesfull.')
+		break;
+	}
+	else{
+		alert("Login is not correct," + " " + userName + ".");
+		break;
+	};
+
+};
+
+
+
+
